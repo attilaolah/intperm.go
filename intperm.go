c8c53825dd6b89019a8ee25ca490b63b3f29db08
Attila Oláh <attilaolah@gmail.com>
2015-03-23 10:55:04 +0100

    make golint happy

--- intperm.go
+++ intperm.go
@@ -6,34 +6,35 @@ package intperm
 // 64-bit full-one bitmask
 const ones = 0xffffffffffffffff
 
-type permutation []uint64
+// A Permutation is a reversible, pseudo-random mapping of 64-bit unsigned ints.
+type Permutation []uint64
 
-// New creates a new permutation.
+// New creates a new Permutation.
 // The argument can be any random number.
-func New(seed uint64) permutation {
+func New(seed uint64) Permutation {
 	masks := make([]uint64, 64*2)
 	params := triplets[seed%uint64(len(triplets))]
 	for i := range masks {
 		seed = xorshift(seed, params[0], params[1], params[2])
 		masks[i] = seed & ((1 << uint64(i>>1)) ^ uint64(ones))
 	}
-	return permutation(masks)
+	return Permutation(masks)
 }
 
 // MapTo a number to another random one.
-func (p permutation) MapTo(x uint64) uint64 {
+func (p Permutation) MapTo(x uint64) uint64 {
 	return p.doMap(x, 0, 64, 1)
 }
 
 // MapFrom is the reverse of MapTo.
 // In other words, p.MapFrom(p.MapTo(x)) == x.
-func (p permutation) MapFrom(x uint64) uint64 {
+func (p Permutation) MapFrom(x uint64) uint64 {
 	return p.doMap(x, 63, -1, -1)
 }
 
 // Used by both MapTo and MapFrom.
 // Set `to` to true for MapTo, and `false` for MapFrom behaviour.
-func (p permutation) doMap(x uint64, from, to, step int) uint64 {
+func (p Permutation) doMap(x uint64, from, to, step int) uint64 {
 	for i := from; i != to; i += step {
 		u := uint64(i)
 		bit := uint64(1 << u)
